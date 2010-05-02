7c3eac6b8cb9a878519cdf0e579f4950274df6c4
unknown <Sarah@.(none)>
2010-05-02 13:33:08 +0200

    Cleaned-up version

--- src/main/java/no/kodedojo/sudoko/Solver.java
+++ src/main/java/no/kodedojo/sudoko/Solver.java
@@ -2,11 +2,6 @@ package no.kodedojo.sudoko;
 
 public class Solver {
     public boolean findSolution(Board board) {
-dsg             sdsd
-sdafg         return false;
-
-
-    sdf}
-
-
-sdgdsg }
+        return false;
+    }
+}
